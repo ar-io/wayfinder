a97574133837185d61e2c1e522d3ea2446a2ebb2
dtfiedler <dylan@ar.io>
2025-07-09 08:53:04 -0500

    chore(core): fix telemetry for chrome extensions

---
+++ packages/core/src/utils/browser.ts
@@ -0,0 +1,36 @@
+/**
+ * WayFinder
+ * Copyright (C) 2022-2025 Permanent Data Solutions, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Returns true if running in a browser environment (window is defined).
+ */
+export function isBrowser(): boolean {
+  return (
+    typeof window !== 'undefined' && typeof window.document !== 'undefined'
+  );
+}
+
+/**
+ * Returns true if running as a Chrome extension (chrome.runtime and chrome.runtime.id are defined).
+ */
+export function isChromeExtension(): boolean {
+  return (
+    typeof chrome !== 'undefined' &&
+    typeof chrome.runtime !== 'undefined' &&
+    typeof chrome.runtime.id === 'string'
+  );
+}
