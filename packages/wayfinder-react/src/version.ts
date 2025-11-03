cc4faa5af385d45b00623a774271e38a1435ad6b
dtfiedler <dfieds913@gmail.com>
2025-11-03 14:27:57 -0600

    chore(x402): fix build for x402

--- packages/wayfinder-react/src/version.ts
+++ packages/wayfinder-react/src/version.ts
@@ -14,4 +14,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export const WAYFINDER_REACT_VERSION = 'v1.0.21';
+export const WAYFINDER_REACT_VERSION = 'v1.0.23-alpha.0';
