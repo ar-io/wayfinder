83f5553c5a34d4289679c57e779bab02d6c7dc92
dtfiedler <dfieds913@gmail.com>
2025-11-10 12:55:26 -0600

    chore(core): lint and format

--- packages/wayfinder-core/src/fetch/wayfinder-fetch.ts
+++ packages/wayfinder-core/src/fetch/wayfinder-fetch.ts
@@ -181,7 +181,7 @@ export const createWayfinderFetch = ({
       }
 
       const requestHeaders = {
-        ...Object.fromEntries(new Headers(init?.headers ?? {})),
+        ...Object.fromEntries(new Headers(init?.headers || {})),
         ...createWayfinderRequestHeaders({
           traceId: requestSpan?.spanContext().traceId,
         }),
